2e8b06c9a77d31e0ef16f39daf3e986fb5d2e330
Saman22178 <indhikajayaruwan52@gmail.com>
2025-08-15 00:25:42 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~kudxGYLC#1LENKnZiaPnPqvLV246LhJJSA-rvN1t6HAUvVxa9ZNM",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "94703781273", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
